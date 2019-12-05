44eb9cdaf46109884eb3d4ca85829e35ee8f0643
José Rubens <rubensbritto15@gmail.com>
2019-12-04 23:39:47 -0300

    Finish

--- estacionamento/src/estacionamento/Main.java
+++ estacionamento/src/estacionamento/Main.java
@@ -1,12 +1,7 @@
 package estacionamento;
-
-//import java.util.ArrayList;
-//import java.util.Scanner;
 import estacionamento.Menu;
 
 public class Main {
-	//ArrayList<Veiculo> veiculos;
-	//public Scanner input = new Scanner(System.in);
 	
 	public static void main(String[] args){
 		Menu menu = new Menu();
